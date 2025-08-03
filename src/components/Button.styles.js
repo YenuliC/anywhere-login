6f876eda5206f1f79c594871fabac620a926fd6b
Yenuli Chamodya <yenulichamodya@gmail.com>
2025-08-03 22:22:08 +0530

    Added styles of  button component

---
+++ src/components/Button.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  padding: 12px 24px;
+  background-color: #007bff;
+  color: white;
+  font-size: 16px;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
